c99db0b77e0090e9812d0fde1b3034a509fe75ee
Melih Gunduz <melih.gunduz@hotmail.com>
2023-10-09 13:41:50 +0300

    some functions fixed

--- RentalContract.sol
+++ RentalContract.sol
@@ -20,7 +20,7 @@ contract RentalContract {
         bytes32 propertyID;
         uint256 startTime;
         uint256 endTime;
-        bool active; // Kiralanabilir durumda mı?
+        bool active;
     }
 
     mapping(bytes32 => Property) public properties;
@@ -40,9 +40,9 @@ contract RentalContract {
         _;
     }
 
+    // Create property for rent
     function createProperty(string memory __type, string memory _adres) public  {
         bytes32 _propertyID = generateHash();
-
 
         properties[_propertyID].owner = msg.sender;
         properties[_propertyID]._type = __type;
@@ -54,7 +54,7 @@ contract RentalContract {
         emit CreateRental(msg.sender, __type,_adres);
     }
 
-
+    // Start rental
     function startRental(string memory tenantName,bytes32 _propertyID, uint256 _startTime, uint256 _endTime) public {
         require(properties[_propertyID].active == true, "This property already in use.");
         require(_startTime < _endTime, "Start time must be small than end time.");
@@ -67,27 +67,35 @@ contract RentalContract {
         emit StartRental(_startTime, _endTime);
     } 
 
+    // Finish rental
     function finishRental(bytes32 _propertyID) external onlyOwner(_propertyID) {
-        require(properties[_propertyID].active, "This property is not rented.");
+        require(!properties[_propertyID].active, "This property is not rented.");
+        require(properties[_propertyID].endTime < block.timestamp, "You can't finish rent now");
 
         Property storage property = properties[_propertyID];
         address TenantAdres = property.owner; // Geçici olarak sahibi al
-        property.active = false;
+        property.active = true;
         delete properties[_propertyID];
 
         emit FinishRental(msg.sender, TenantAdres, property.propertyAddress);
     }
 
+    // generate uinque hash for property id
     function generateHash() private returns(bytes32) {
         counter += 1;
         return keccak256(abi.encodePacked(counter + block.timestamp)); // Hash generated and returned
+    }
+
+    // return all properties ids
+    function getPropertyIDs() public view returns(bytes32[] memory) {
+        return PropertyIDs;
     }
 
     function checkRentStatus(bytes32 propertyID) external view returns (bool) {
         return properties[propertyID].active && block.timestamp >= properties[propertyID].startTime && block.timestamp <= properties[propertyID].endTime;
     }
 
-    function hataliDurumuBildir() external onlyTenant {
-        // Hatalı durumu bildirme işlemleri burada gerçekleştirilebilir
+    function report() external onlyTenant {
+        // reporting error situations
     }
 }
