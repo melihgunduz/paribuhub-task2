a83fcd030411f2eec31e2fce274269b90686433b
Melih Gunduz <melih.gunduz@hotmail.com>
2023-10-09 13:31:56 +0300

    functions created

--- RentalContract.sol
+++ RentalContract.sol
@@ -1,39 +1,81 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-contract ParibuHubTask2 {
+contract RentalContract {
 
-    address public owner;
+    uint256 counter;
 
-    //mapping (address => Property[]) public userProperties;
-
-    Property[] public properties;
-
-    uint256 public counter;
+    bytes32[] public PropertyIDs;
+    
+    struct Tenant {
+        address _address;
+        string name;
+    }
 
     struct Property {
-        address customer;
+        Tenant tenant;
         address owner;
-        bytes32 id;
-        string name;
         string _type;
-        uint256 price;
+        string propertyAddress;
+        bytes32 propertyID;
+        uint256 startTime;
+        uint256 endTime;
+        bool active; // Kiralanabilir durumda mı?
     }
 
-    constructor() {
-        owner = msg.sender;
-        counter = 0;
+    mapping(bytes32 => Property) public properties;
+    mapping(address => Tenant) public tenants;
+
+    event CreateRental(address indexed owner, string indexed _type, string indexed adres);
+    event StartRental(uint256 startTime, uint256 endTime);
+    event FinishRental(address indexed owner, address indexed Tenant, string indexed adres);
+
+    modifier onlyOwner(bytes32 _propertyID) {
+        require(msg.sender == properties[_propertyID].owner, "Only owner can run this function.");
+        _;
     }
 
-    function createProperty (string memory _name, string memory __type, uint256 _price) public {
-        bytes32 id = generateHash();
-        Property memory property;
-        property.owner = msg.sender;
-        property.id = id;
-        property.name = _name;
-        property._type = __type;
-        property.price = _price;
-        properties.push(property);
+    modifier onlyTenant() {
+        require(tenants[msg.sender]._address != address(0), "Only tenant can run this function.");
+        _;
+    }
+
+    function createProperty(string memory __type, string memory _adres) public  {
+        bytes32 _propertyID = generateHash();
+
+
+        properties[_propertyID].owner = msg.sender;
+        properties[_propertyID]._type = __type;
+        properties[_propertyID].propertyAddress = _adres;
+        properties[_propertyID].propertyID = _propertyID;
+        properties[_propertyID].active = true;
+
+        PropertyIDs.push(_propertyID);
+        emit CreateRental(msg.sender, __type,_adres);
+    }
+
+
+    function startRental(string memory tenantName,bytes32 _propertyID, uint256 _startTime, uint256 _endTime) public {
+        require(properties[_propertyID].active == true, "This property already in use.");
+        require(_startTime < _endTime, "Start time must be small than end time.");
+
+        properties[_propertyID].startTime = block.timestamp + _startTime;
+        properties[_propertyID].endTime = block.timestamp + _endTime;
+        properties[_propertyID].active = false;
+        properties[_propertyID].tenant._address = msg.sender;
+        properties[_propertyID].tenant.name = tenantName;
+        emit StartRental(_startTime, _endTime);
+    } 
+
+    function finishRental(bytes32 _propertyID) external onlyOwner(_propertyID) {
+        require(properties[_propertyID].active, "This property is not rented.");
+
+        Property storage property = properties[_propertyID];
+        address TenantAdres = property.owner; // Geçici olarak sahibi al
+        property.active = false;
+        delete properties[_propertyID];
+
+        emit FinishRental(msg.sender, TenantAdres, property.propertyAddress);
     }
 
     function generateHash() private returns(bytes32) {
@@ -41,8 +83,11 @@ contract ParibuHubTask2 {
         return keccak256(abi.encodePacked(counter + block.timestamp)); // Hash generated and returned
     }
 
-    receive() external payable {
-        revert();
-     }
+    function checkRentStatus(bytes32 propertyID) external view returns (bool) {
+        return properties[propertyID].active && block.timestamp >= properties[propertyID].startTime && block.timestamp <= properties[propertyID].endTime;
+    }
 
+    function hataliDurumuBildir() external onlyTenant {
+        // Hatalı durumu bildirme işlemleri burada gerçekleştirilebilir
+    }
 }
